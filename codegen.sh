770a362674938669abbacf1bbf1fd29259917c3b
Richard Gross <richard.gross@maibornwolff.de>
2022-06-09 22:37:48 +0200

    add build step to code generation

--- codegen.sh
+++ codegen.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
 rm -rf generated/api
 mkdir -p generated/api
 
 npx openapi-generator-cli generate
 
-#rm generated/api/index.ts
-#rm generated/api/runtime.ts
+cd generated/api || exit
+npm run build
+cd ../..
